bc7d88efe58ffe96e8899a0ad7756ab04f8dbe9e
bogus <bogusshtudy@gmail.com>
2025-06-27 11:01:28 +0530

    artifacts test

--- show-ev-vars.py
+++ show-ev-vars.py
@@ -4,5 +4,5 @@ print('Env var GLOBAL_VAR_1 =', os.environ.get('GLOBAL_VAR_1', 'value not found'
 print('Env var GLOBAL_VAR_2 =', os.environ.get('GLOBAL_VAR_2', 'value not found'))
 print('Env var FIRST_JOB_ENV_1 =', os.environ.get('FIRST_JOB_ENV_1', 'value not found'))
 print('Env var FIRST_JOB_ENV_2 =', os.environ.get('FIRST_JOB_ENV_2', 'value not found'))
-print('Env var GLOBAL_VAR_1 =', os.environ.get('GLOBAL_VAR_1', 'value not found'))
-print('Env var GLOBAL_VAR_1 =', os.environ.get('GLOBAL_VAR_1', 'value not found'))
+print('Env var SECOND_JOB_ENV_1 =', os.environ.get('SECOND_JOB_ENV_1', 'value not found'))
+print('Env var SECOND_JOB_ENV_2 =', os.environ.get('SECOND_JOB_ENV_2', 'value not found'))
